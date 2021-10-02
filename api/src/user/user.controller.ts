58825fac80119f5c4738a7da9b92f7d89225a036
shawn_mccall <mccalls2@yahoo.com>
2021-10-02 14:48:35 -0400

    video-02 create user feature module, serivce, controller, userEntity, userInterface

---
+++ api/src/user/user.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { User } from './models/user.interface';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+
+    constructor(private readonly userService: UserService) {}
+
+    @Post()
+    create(@Body() user: User): Observable<User> {
+        return this.userService.create(user)
+    }
+    
+    @Get(':id')
+    findOne(@Param('id') id: string): Observable<User> {
+        return this.userService.findOne(+id)
+    }
+
+    @Get()
+    findAll():Observable<User[]> {
+        return this.userService.findAll()
+    }
+
+    @Delete(':id')
+    deleteOne(@Param('id') id: string): Observable<any> {
+        return this.userService.deleteOne(+id)
+    }
+
+    @Put(':id')
+    updateOne( @Param('id') id: string, @Body() user: User): Observable<any> {
+        return this.userService.updateOne(+id, user)
+    }
+}
+
