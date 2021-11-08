232eac61b3d68e282a08ab9d00adaccb8caaf26f
shawn_mccall <mccalls2@yahoo.com>
2021-11-08 14:26:15 -0500

    clean up and add unit tests for auth guard frontend

--- frontend/src/app/components/login/login.component.spec.ts
+++ frontend/src/app/components/login/login.component.spec.ts
@@ -1,9 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { Observable, of, throwError } from 'rxjs';
+import { of, throwError } from 'rxjs';
 import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
 
 import { LoginComponent } from './login.component';
--- frontend/src/app/guards/auth.guard.spec.ts
+++ frontend/src/app/guards/auth.guard.spec.ts
@@ -1,16 +1,51 @@
 import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthenticationService } from '../services/authentication/authentication.service';
 
 import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
+  let router: Router;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
+  let authService: AuthenticationService 
+  let authServiceStub = {
+    isAuthenticated: jasmine.createSpy()
+  }
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports:[RouterTestingModule],
+      providers: [{ provide: AuthenticationService, useValue: authServiceStub}]
+    }).compileComponents()
+
     guard = TestBed.inject(AuthGuard);
+    router = TestBed.inject(Router)
+    authService = TestBed.inject(AuthenticationService)
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
   });
+
+  it('should navigate to the login when not authenticated', () => {
+    authServiceStub.isAuthenticated.and.returnValue(false)
+    spyOn(router, 'navigate').and.stub()
+
+    const canActive = guard.canActivate()
+
+    expect(router.navigate).toHaveBeenCalledWith(['login'])
+    expect(canActive).toBeFalse()
+  }) 
+  
+  it('should return true when the user is authenticated', () => {
+    authServiceStub.isAuthenticated.and.returnValue(true)
+    spyOn(router, 'navigate').and.stub()
+
+    const canActive = guard.canActivate()
+
+    expect(router.navigate).not.toHaveBeenCalled()
+    expect(canActive).toBeTrue()
+  })
 });
