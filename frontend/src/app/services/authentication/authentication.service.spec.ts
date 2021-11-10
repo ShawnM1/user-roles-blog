a578cefc878bc731ab182f144c6648e44dee9ad6
shawn_mccall <mccalls2@yahoo.com>
2021-11-09 22:33:05 -0500

    unit tests for user service and auth service and jwt interceptor

--- frontend/src/app/services/authentication/authentication.service.spec.ts
+++ frontend/src/app/services/authentication/authentication.service.spec.ts
@@ -1,16 +1,99 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
+import { of } from 'rxjs';
+import { User } from 'src/app/model/user.interface';
 
-import { AuthenticationService } from './authentication.service';
+import { AuthenticationService, JWT_NAME } from './authentication.service';
 
 describe('AuthenticationService', () => {
-  let service: AuthenticationService;
+  let authService: AuthenticationService;
+  let httpClientSpy = jasmine.createSpyObj('HttpClient', ['post'])
+  let jwtHelperService: JwtHelperService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthenticationService);
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: HttpClient, useValue: httpClientSpy },
+        { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
+        JwtHelperService], 
+    });
+    authService = TestBed.inject(AuthenticationService);
+    jwtHelperService = TestBed.inject(JwtHelperService)
   });
 
+  afterEach(() => {
+    httpClientSpy.post.calls.reset()
+  })
+
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(authService).toBeTruthy();
+
   });
+
+  describe('login', () => {
+    it('should make a POST request to the api/auth/login endpoint and store the token in local storage', () => {
+      const loginForm = { email: 'email', password: 'pw'}
+      const tokenObj = { access_token: 'token' }
+      httpClientSpy.post.and.returnValue(of(tokenObj))
+      spyOn(localStorage, 'setItem').and.stub()
+
+      authService.login(loginForm).subscribe((token: string) => {
+        expect(tokenObj).toEqual(tokenObj)
+        expect(localStorage.setItem).toHaveBeenCalledWith(JWT_NAME, tokenObj.access_token)
+      })
+      expect(httpClientSpy.post).toHaveBeenCalledWith('/api/auth/login', loginForm)
+    })
+  })
+
+  describe('logout', () => {
+    it('should remove the JWT from local storage', () => {
+      spyOn(localStorage, 'removeItem').and.stub()
+
+      authService.loguout()
+      expect(localStorage.removeItem).toHaveBeenCalledWith(JWT_NAME)
+    })
+  })
+
+  describe('register', () => {
+    it('should make a POST request to the /api/users endpoint', () => {
+      const user = {} as User
+      authService.register(user).subscribe()
+      expect(httpClientSpy.post).toHaveBeenCalledWith('/api/users', user)
+    })
+  })
+
+  describe('getUserId', () => {
+   it('should return the userId', () => {
+    spyOn(localStorage, 'getItem').and.returnValue('jwt')
+    const decodedJwtMock = { sub: 1 }
+    spyOn(jwtHelperService, 'decodeToken').and.returnValue(decodedJwtMock)
+
+    authService.getUserId().subscribe((id: number) => {
+      expect(id).toEqual(decodedJwtMock.sub)
+    })
+    expect(localStorage.getItem).toHaveBeenCalledWith(JWT_NAME)
+    expect(jwtHelperService.decodeToken).toHaveBeenCalledWith('jwt')
+   })
+  })
+
+  describe('isAuthenticated', () => {
+    it('should return true if the token is not expired', () => {
+     spyOn(localStorage, 'getItem').and.returnValue('jwt')
+     spyOn(jwtHelperService, 'isTokenExpired').and.returnValue(false)
+     const actual = authService.isAuthenticated()
+
+     expect(localStorage.getItem).toHaveBeenCalledWith(JWT_NAME)
+     expect(actual).toBeTrue()
+    })
+
+    it('should return false if the token is not expired', () => {
+      spyOn(localStorage, 'getItem').and.returnValue('jwt')
+      spyOn(jwtHelperService, 'isTokenExpired').and.returnValue(true)
+      const actual = authService.isAuthenticated()
+ 
+      expect(localStorage.getItem).toHaveBeenCalledWith(JWT_NAME)
+      expect(actual).toBeFalse()
+     })
+  })
 });
--- frontend/src/app/services/user/user.service.spec.ts
+++ frontend/src/app/services/user/user.service.spec.ts
@@ -1,16 +1,85 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { User } from 'src/app/model/user.interface';
 
-import { UserService } from './user.service';
+import { UserData, UserService } from './user.service';
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
+  let httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'put', 'post', 'delete']);
+
+  const userMock: User = {
+    id: 1,
+    name: 'name',
+    username: 'username',
+    email: 'mail.com',
+    role: 'ADMIN',
+
+  }
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
+    TestBed.configureTestingModule({
+      providers: [{ provide: HttpClient, useValue: httpClientSpy}]
+    });
+    userService = TestBed.inject(UserService);
+
+    httpClientSpy.get.calls.reset()
+    httpClientSpy.put.calls.reset()
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(userService).toBeTruthy();
   });
+
+  describe('findOne', () => {
+    it('should make GET request to the api', () => {
+      httpClientSpy.get.and.returnValue(of(userMock))
+      const userId = 1
+
+      userService.findOne(userId).subscribe((user: User) => {
+        expect(user).toEqual(userMock)
+      })
+      expect(httpClientSpy.get).toHaveBeenCalledWith(`/api/users/${userId}`)
+    })
+  })
+
+  describe('updateOne', () => {
+    it('should make a PUT request to the api', () => {
+      httpClientSpy.put.and.returnValue(of(userMock))
+
+      userService.updateOne(userMock).subscribe((user: User) => {
+        expect(user).toEqual(userMock)
+      })
+      expect(httpClientSpy.put).toHaveBeenCalledWith(`api/users/${userMock.id}`, userMock)
+    })
+  })
+
+  describe('findAll', () => {
+    it('should return UserData', () => {
+      const userDataMock  = {
+        items: [userMock]
+      } as UserData
+
+      let params = new HttpParams().append('page', String(1)).append('limit', String(2))
+      httpClientSpy.get.and.returnValue(of(userDataMock))
+
+      userService.findAll(1, 2).subscribe((userData: UserData) => {
+        expect(userData).toEqual(userDataMock)
+      })
+
+      expect(httpClientSpy.get).toHaveBeenCalledWith('/api/users', { params })
+    })
+  })
+
+  describe('updateProfileImage', () => {
+    it('should make a POST request to the api', () => {
+      const formData: FormData = {} as FormData
+      const options =  { reportProgress: true, observe: 'events'}
+
+      userService.uploadProfileImage(formData)
+
+      expect(httpClientSpy.post).toHaveBeenCalledWith('/api/users/upload', formData, options)
+    })
+  })
 });
