af3f51266d6e0c6a8ecdb40de8055aa55eb76a35
shawn_mccall <mccalls2@yahoo.com>
2021-11-11 20:29:26 -0500

    blog controller unit tests

--- api/src/blog/controller/blog.controller.spec.ts
+++ api/src/blog/controller/blog.controller.spec.ts
@@ -22,10 +22,9 @@ describe('BlogController', () => {
       providers: [{ provide: BlogService, useValue: { 
         create: jest.fn().mockReturnValue(of(blogEntryMock)),
         findAll: jest.fn(),
-        paginateAll: jest.fn(),
-        paginateByUser: jest.fn(),
-        findByUser: jest.fn(),
-        findOne: jest.fn(),
+        paginateAll: jest.fn().mockReturnValue(of('')),
+        paginateByUser: jest.fn().mockReturnValue(of('')),
+        findOne: jest.fn().mockReturnValue(of('')),
         updateOne: jest.fn(),
         deleteOne: jest.fn()
       }}]
@@ -48,6 +47,65 @@ describe('BlogController', () => {
         expect(blogEntry).toEqual(blogEntryMock)
       })
       expect(blogService.create).toHaveBeenCalledWith(userMock, blogEntryMock)
+    })
+  })
+
+  describe('index', () => {
+    it('should call paginateAll from blogService', () => {
+      const page = 1
+      const limit = 10
+      controller.index(page, limit).subscribe()
+      expect(blogService.paginateAll).toHaveBeenCalledWith({ page, limit })
+    })
+
+    it('should default the limit to 100 when the limit argument is over 100', () => {
+      const page = 1
+      const overLimit = 101
+      controller.index(page, overLimit).subscribe()
+      expect(blogService.paginateAll).toHaveBeenCalledWith({ page, limit: 100 })
+    })
+  })
+
+  describe('indexByUser', () => {
+    it('should call paginateAll from blogService', () => {
+      const userId = 7
+      const page = 1
+      const limit = 10
+      controller.indexByUser(page, limit, userId).subscribe()
+      expect(blogService.paginateByUser).toHaveBeenCalledWith({ page, limit }, userId)
+    })
+
+    it('should default the limit to 100 when the limit argument is over 100', () => {
+      const userId = 7
+      const page = 1
+      const overLimit = 101
+      controller.indexByUser(page, overLimit, userId).subscribe()
+      expect(blogService.paginateByUser).toHaveBeenCalledWith({ page, limit: 100 }, userId)
+    })
+  })
+
+  describe('findOne', () => {
+    it('should call the blogService findOne method', () => {
+      const id = 1;
+      controller.findOne(1).subscribe()
+      expect(blogService.findOne).toHaveBeenCalledWith(id)
+    })
+  })
+
+  describe('updateOne', () => {
+    it('should call the blogService updateOne method', () => {
+      const blogEntryToUpdate = {} as BlogEntry
+      const id = 1
+      controller.updateOne(id, blogEntryToUpdate)
+      expect(blogService.updateOne).toHaveBeenCalledWith(id, blogEntryToUpdate)
+    })
+  })
+
+  describe('deleteOne', () => {
+    it('should call the blogService deleteOne method', () => {
+      const id = 1
+      controller.deleteOne(id)
+      expect(blogService.deleteOne).toHaveBeenCalledWith(id)
     })
   })
 });
