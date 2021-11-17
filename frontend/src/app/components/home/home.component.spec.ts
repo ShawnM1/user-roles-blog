9c18f450512ea5f5efa30716c841bafc277319f0
shawn_mccall <mccalls2@yahoo.com>
2021-11-16 20:56:19 -0500

    home component spec unit test

--- frontend/src/app/components/home/home.component.spec.ts
+++ frontend/src/app/components/home/home.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PageEvent } from '@angular/material/paginator';
 import { BlogService } from 'src/app/services/blog/blog.service';
 
 import { HomeComponent } from './home.component';
@@ -6,12 +7,12 @@ import { HomeComponent } from './home.component';
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
-  let blogService: BlogService
+  let blogService: any
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ HomeComponent ],
-      providers: [BlogService]
+      providers: [{ provide: BlogService, useValue: { indexAll: jasmine.createSpy().and.stub()}}]
     })
     .compileComponents();
 
@@ -26,4 +27,12 @@ describe('HomeComponent', () => {
     expect(component).toBeTruthy();
   });
 
-});
+  describe('onPaginateChange', () => {
+    it('should call the blogService indexAll method with the pageIndex and pageSize twice', () => {
+      const pageEvent = { pageSize: 1, pageIndex: 2} as PageEvent
+      component.onPaginateChange(pageEvent)
+      expect(blogService.indexAll).toHaveBeenCalledTimes(2)
+    })
+  })
+})
+
